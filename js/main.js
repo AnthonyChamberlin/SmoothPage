becad90ae20e31e07aab39901ffc6869923731df
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-01-24 11:59:29 +0000

    bug fixes

--- js/main.js
+++ js/main.js
@@ -13,15 +13,6 @@ jQuery(document).ready(function(event){
     firstLoad = true;
   });
 
-  $('main').on('click', '[data-type="horizontal-page-transition"]', function(event){
-    event.preventDefault();
-    //detect which page has been selected
-    var newPage = $(this).attr('href');
-    //if the page is not already being animated - trigger animation
-    if( !isAnimating ) horizontalPageChange(newPage, true);
-    firstLoad = true;
-  });
-
   //detect the 'popstate' event - e.g. user clicking the back button
   $(window).on('popstate', function() {
   	if( firstLoad ) {
@@ -34,7 +25,6 @@ jQuery(document).ready(function(event){
         newPage = newPageArray[newPageArray.length - 1];
 
       if( !isAnimating  &&  newLocation != newPage ) verticalPageChange(newPage, false);
-      if( !isAnimating  &&  newLocation != newPage ) horizontalPageChange(newPage, false);
     }
     firstLoad = true;
 	});
@@ -55,21 +45,6 @@ jQuery(document).ready(function(event){
     }
 	}
 
-  function horizontalPageChange(url, bool) {
-    isAnimating = true;
-    // trigger page animation
-    $('.horizontal-slide').addClass('page-is-changing');
-    $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-    	loadNewContent(url, bool);
-      newLocation = url;
-      $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-    });
-    //if browser doesn't support CSS transitions
-    if( !transitionsSupported() ) {
-      loadNewContent(url, bool);
-      newLocation = url;
-    }
-	}
 
 	function loadNewContent(url, bool) {
 		url = ('' == url) ? 'home.html' : url;
@@ -82,8 +57,6 @@ jQuery(document).ready(function(event){
       //if browser doesn't support CSS transitions - dont wait for the end of transitions
       var delay = ( transitionsSupported() ) ? 1400 : 0;
 
-      if ( section.hasClass('cd-vertical') ) {
-
         setTimeout(function(){
           //wait for the end of the transition on the loading bar before revealing the new content
           ( section.hasClass('cd-vertical') ) ? $('body').addClass('cd-vertical') : $('body').removeClass('cd-vertical');
@@ -94,23 +67,6 @@ jQuery(document).ready(function(event){
           });
           if( !transitionsSupported() ) isAnimating = false;
         }, delay);
-
-      } else if (section.hasClass('cd-horizontal')) {
-
-        setTimeout(function(){
-          //wait for the end of the transition on the loading bar before revealing the new content
-          ( section.hasClass('cd-horizontal') ) ? $('body').addClass('cd-horizontal') : $('body').removeClass('cd-horizontal');
-          $('body').removeClass('page-is-changing');
-          $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-            isAnimating = false;
-            $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-          });
-
-          if( !transitionsSupported() ) isAnimating = false;
-        }, delay);
-      }
-
-
 
       if(url!=window.location && bool){
         //add the new page to the window.history
